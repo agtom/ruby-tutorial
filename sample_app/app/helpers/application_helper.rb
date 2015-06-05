377a14f03ba73ee0a9d164650b491436b40ee1bb
Andrew Tomlinson <andrewtomlinson@Andrews-MacBook-Air.local>
2015-06-05 13:44:03 -0400

    Modify and style the Home Page

--- sample_app/app/helpers/application_helper.rb
+++ sample_app/app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+	# methods defined here are available in views of our application
+	# Returns full title on a per-page basis
+	# page_title = '' gives page_title a default value of an empty string
+	def full_title(page_title = '')
+		base_title = "Sample App"
+		if page_title.empty?
+			base_title
+		else
+			"#{page_title} | #{base_title}"
+		end
+	end
 end
