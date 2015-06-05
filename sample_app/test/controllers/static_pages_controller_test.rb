f5fc16a0696f5cac106d40ebf03db33bd0003f13
Andrew Tomlinson <andrewtomlinson@Andrews-MacBook-Air.local>
2015-06-05 16:12:26 -0400

    FInish layout and routes

--- sample_app/test/controllers/static_pages_controller_test.rb
+++ sample_app/test/controllers/static_pages_controller_test.rb
@@ -14,9 +14,15 @@ class StaticPagesControllerTest < ActionController::TestCase
   end
 
   test "should get about" do
-  	get :about
-  	assert_response :success
+    get :about
+    assert_response :success
     assert_select "title", "About | Sample App"
+  end
+
+  test "should get contact" do
+    get :contact
+    assert_response :success
+    assert_select "title", "Contact | Sample App"
   end
 
 end
